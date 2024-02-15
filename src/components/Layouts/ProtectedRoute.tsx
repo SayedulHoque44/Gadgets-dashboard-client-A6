1c5e11ecbaec238c062f66a8cc9bb3463e4e0304
sayedulhoque44 <sayedulhoque3544@gmail.com>
2024-02-15 21:46:37 +0600

    routed updated

--- src/components/Layouts/ProtectedRoute.tsx
+++ src/components/Layouts/ProtectedRoute.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useCurrentToken } from "../../redux/features/Auth/authSlice";
 import { useAppSelector } from "../../redux/hooks";
-import { verifyToken } from "../../utils/verifyToken";
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const token = useAppSelector(useCurrentToken);
@@ -10,8 +9,7 @@ const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   if (!token) {
     return <Navigate to="/login" replace={true} />;
   }
-  const user = verifyToken(token);
-  console.log(user);
+
   return children;
 };
 
